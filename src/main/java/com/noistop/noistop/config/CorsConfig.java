65cc2d1dd571dfa159823e41cecea5a7b5e8dd35
Fabrizzio <fabri_gam12@hotmail.com>
2026-07-04 11:08:39 -0500

    Actualizar configuración CORS para frontend desplegado

--- src/main/java/com/noistop/noistop/config/CorsConfig.java
+++ src/main/java/com/noistop/noistop/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "https://noistop-frontend.onrender.com")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
